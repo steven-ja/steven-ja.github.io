164c0b47becd9fec2435f987a47b662520098dfb
Stefano <stefanokx97@gmail.com>
2024-07-25 01:16:25 +0200

    added cached functions

--- content/posts/renewables/world-energy/main.py
+++ content/posts/renewables/world-energy/main.py
@@ -23,10 +23,65 @@ def load_data():
         'nuclear_electricity' : "Nuclear"
     }, inplace=True)
 
-    df.drop(columns=[])
+    df = df[~df.country.isin([
+        "G20 (Ember)",
+        "OECD (Ember)",
+        "Asia (Ember)",
+        "G7 (Ember)",
+        "OECD (EI)",
+        "Asia Pacific (EI)",
+        "High-income countries",
+        "Europe (Ember)",
+        "Non-OECD (EI)",
+        "Asia",
+        "North America (Ember)",
+        "Upper-middle-income countries",
+        "Europe (EI)",
+        "Oceania (Ember)",
+        "ASEAN (Ember)",
+        "Lower-middle-income countries", 
+        "European Union (27)", 
+        "North America (EI)"
+    ])]
     
     return df.sort_values('year')
 
+
+@st.cache_data
+def load_world_data(map_data):
+    world = gpd.read_file("ne_110m_admin_0_countries/ne_110m_admin_0_countries.dbf")
+# data processing
+    world.rename(columns={"ADMIN": "country"}, inplace=True)
+# replace country name to fit the other dataset
+    world.country = world.country.replace(
+        ["United States of America", "Democratic Republic of the Congo", "Republic of the Congo", "United Republic of Tanzania", "The Bahamas", "Czechia", "eSwatini", "Republic of Serbia"], 
+        ["United States", "Democratic Republic of Congo", "Congo", "Tanzania", "Bahamas", "Czechoslovakia", "Eswatini", "Serbia"]
+    )
+
+    # world.SOV_A3 = world.SOV_A3.replace(["US1", "CH1", "FR1", "KA1", "GB1", "NZ1", "AU1"], ["USA", "CHN", "FRA", "KAZ", "GBR", "NZL", "AUS"])
+    # latest_year = recent_data['year'].max() - pd.Timedelta(days=365*2)
+    # latest_data = recent_data[recent_data['year'] == latest_year]
+    world = world.merge(map_data, on=['country'])
+    return world
+
+
+@st.cache_data
+def get_map_plot(energy_type_map, year_map):
+    match energy_type_map:
+        case "Solar":
+            color_scale = "Oranges"
+        case "Wind":
+            color_scale = "Greens"
+        case "Hydro":
+            color_scale = "Blues"
+        case _ :
+            color_scale = "Viridis"
+
+    fig = px.choropleth(world, locations='ADM0_A3', color=energy_type_map, 
+                    hover_name='country', projection='natural earth2', color_continuous_scale=color_scale,
+                    title=f'{energy_type_map.replace("_", " ").title()} Production in {year_map}')
+
+    return fig
 df = load_data()
 
 
@@ -41,7 +96,7 @@ end_year = st.sidebar.slider('End Year', 1900, 2022, 2023)
 
 # Filter data based on selected years
 filtered_df = df[(df['year'].dt.year >= start_year) & (df['year'].dt.year <= end_year)]
-# st.write(filtered_df)
+
 # Global trend plot
 st.header('Global Energy Production Trends')
 energy_types = st.multiselect(
@@ -60,33 +115,22 @@ st.plotly_chart(fig)
 # Map of energy production
 st.header('Energy Production Map')
 energy_type_map = st.selectbox('Select Energy Type for Map', energy_types)
-year_map = st.slider('Select Year for Map', start_year, end_year, end_year)
+year_map = st.slider('Select Year for Map', start_year, end_year, end_year-10)
+
 
 map_data = filtered_df[filtered_df['year'].dt.year == year_map]
-world = gpd.read_file("ne_110m_admin_0_countries/ne_110m_admin_0_countries.dbf")
-# data processing
-world.rename(columns={"ADMIN": "country"}, inplace=True)
-# replace country name to fit the other dataset
-world.country = world.country.replace(
-    ["United States of America", "Democratic Republic of the Congo", "Republic of the Congo", "United Republic of Tanzania", "The Bahamas", "Czechia", "eSwatini", "Republic of Serbia"], 
-    ["United States", "Democratic Republic of Congo", "Congo", "Tanzania", "Bahamas", "Czechoslovakia", "Eswatini", "Serbia"]
-    )
+world = load_world_data(map_data)
 
-# world.SOV_A3 = world.SOV_A3.replace(["US1", "CH1", "FR1", "KA1", "GB1", "NZ1", "AU1"], ["USA", "CHN", "FRA", "KAZ", "GBR", "NZL", "AUS"])
-# latest_year = recent_data['year'].max() - pd.Timedelta(days=365*2)
-# latest_data = recent_data[recent_data['year'] == latest_year]
-world = world.merge(map_data, on=['country'])
-
-fig = px.choropleth(world, locations='ADM0_A3', color=energy_type_map, 
-                    hover_name='country', projection='natural earth2', color_continuous_scale='Viridis',
-                    title=f'{energy_type_map.replace("_", " ").title()} Production in {year_map}')
-# fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+fig = get_map_plot(energy_type_map, year_map)
+fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
 st.plotly_chart(fig)
 
 # Top countries comparison
 st.header('Top Countries Comparison')
-n_countries = st.slider('Number of top countries to compare', 1, 10, 5)
+n_countries = st.slider('Number of top countries to compare', 1, 20, 15)
 top_countries = filtered_df.groupby('country')[energy_types[0]].mean().nlargest(n_countries).index.tolist()
+
+st.write(top_countries)
 
 top_countries_data = filtered_df[filtered_df['country'].isin(top_countries)]
 
@@ -96,21 +140,23 @@ st.plotly_chart(fig)
 
 # Energy mix comparison
 st.header('Energy Mix Comparison')
-selected_country = st.selectbox('Select a Country', df['country'].unique(), index=None,
-   placeholder="Select a country ...",)
+selected_country = st.selectbox('Select a Country', df['country'].unique(), index=None)
+if selected_country == None:
+    selected_country = "Italy"
+
 country_data = filtered_df[filtered_df['country'] == selected_country]
-st.write(country_data)
+# st.write(country_data)
 
 
 energy_mix = country_data[energy_types]
-# energy_mix = energy_mix.div(energy_mix.sum(axis=1), axis=0) * 100
 energy_mix['year'] = country_data.year
 
 fig = px.area(energy_mix.dropna(), x='year', y=energy_types,
               title=f'Energy Mix for {selected_country}')
 fig.update_yaxes(title="Electricity Production (TWh)")
 st.plotly_chart(fig)
-st.write(energy_mix)
+# st.write(energy_mix)
+
 
 st.write("""
 This Streamlit app provides an interactive analysis of global energy production trends. 
